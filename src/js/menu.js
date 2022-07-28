748973120dd564d045bd30da502e137b3e8b7ce4
Hanna-alex <ipantevaanna@gmail.com>
2022-07-29 00:47:38 +0600

    create burger menu html, css, js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,31 @@
-const menuBtn = document.querySelector('.header__menu')
-const menu = document.querySelector('.menu')
+function menu() {
+  const btnOpen = document.querySelector('.header__menu-btn')
+  const btnClose = document.querySelector('.menu__btn-clouse')
+  const menu = document.querySelector('.menu')
+  const bodyLock = document.querySelector('.wrapper')
 
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('header__menu--active')
-})
+
+  const openMenu = () => {
+    menu.classList.add('menu-vizible')
+    bodyLock.classList.add('lock')
+  }
+
+
+  const closeMenu = () => {
+    menu.classList.remove('menu-vizible')
+    bodyLock.classList.remove('lock')
+  }
+
+
+  btnOpen.addEventListener('click', openMenu)
+  btnClose.addEventListener('click', closeMenu)
+
+  menu.addEventListener('click', (event) => {
+    console.log(event.target);
+    const target = event.target
+
+    if (target.classList.contains('nav__link')) closeMenu()
+  })
+}
+
+menu()
